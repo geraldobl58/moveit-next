a00a3ff6648cbec16c254f4b3d44f8c745a2f7a7
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-01 16:36:50 -0300

    Feature: add context countdown

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,27 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+    const { 
+        activeChallenge, 
+        resetChallenge, 
+        completeChallenge 
+    } = useContext(ChallengesContext);
+
+    const { resetCountdown } = useContext(CountdownContext);
+
+    function handleChallengeSucceeded() {
+        completeChallenge();
+        resetCountdown();
+    }
+
+    function handleChallengeFailed() {
+        resetChallenge();
+        resetCountdown();
+    }
 
     return (
         <div className={styles.challengeBoxContainer}>
@@ -20,13 +37,14 @@ export function ChallengeBox() {
                         <button
                             type="button"
                             className={styles.challengeFailedButton}
-                            onClick={resetChallenge}
+                            onClick={handleChallengeFailed}
                         >
                                 Falhei
                         </button>
                         <button
                             type="button" 
                             className={styles.challengeSucceededButton}
+                            onClick={handleChallengeSucceeded}
                         >
                                 Completei
                         </button>
@@ -37,7 +55,7 @@ export function ChallengeBox() {
                     <strong>Finalize um ciclo para receber um desafio</strong>
                     <p>
                         <img src="icons/level-up.svg" alt="Level Up"/>
-                        Avance de cevel complentando os desafios.
+                        Avance de level complentando os desafios.
                     </p>
                 </div>
             )}
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/22802518?s=460&v=4" alt="Profile"/>
@@ -8,7 +13,7 @@ export function Profile() {
                <strong>Geraldo Luiz</strong>
                <p>
                    <img src="icons/level.svg" alt="Level"/>
-                   Level 1
+                   Level {level}
                 </p>
             </div>
         </div>
